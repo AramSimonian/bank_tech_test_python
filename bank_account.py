038a055108f3085600d3669dcbb462b0465a4910
Aram Simonian <aram.contacts@gmail.com>
2018-02-06 15:18:09 +0000

    Added a test for zero balance

--- bank_account.py
+++ bank_account.py
@@ -1,10 +1,19 @@
 class BankAccount(object):
   def __init__(self):
     self.__transactions = []
+    self.__balance = 0
 
   @property
   def transactions(self):
     return self.__transactions
 
+  @property
+  def balance(self):
+    return self.__balance
+
+  def alter_balance(self, amount):
+    self.__balance += amount
+
   def add_transaction(self, transaction):
+    self.alter_balance(transaction)
     self.__transactions.append(transaction)
--- tests/test_bank_account.py
+++ tests/test_bank_account.py
@@ -3,8 +3,12 @@ from bank_account import *
 
 class BankAccountTest(unittest.TestCase):
 
+  def test_zero_starting_balance(self):
+    account = BankAccount()
+    self.assertEqual(0, account.balance)
+
   def test_add_transaction(self):
     account = BankAccount()
-    transaction = 'test transaction value'
+    transaction = 5
     account.add_transaction(transaction)
     self.assertEqual(1, len(account.transactions))
